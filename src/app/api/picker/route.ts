2906d8e6347074caa92fa9037eb46cfa3cf40fe1
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-04 20:58:40 +0800

    adding user validation

--- src/app/api/picker/route.ts
+++ src/app/api/picker/route.ts
@@ -4,6 +4,40 @@ import { NextRequest, NextResponse } from 'next/server'
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json()
 
+  const binaryData = new Uint8Array(
+    body.trustedData.messageBytes
+      .match(/.{1,2}/g)!
+      .map((byte) => parseInt(byte, 16))
+  )
+
+  const response = await fetch('https://hub.pinata.cloud/v1/validateMessage', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/octet-stream' },
+    body: binaryData,
+  }).then((r) => r.json())
+
+  const isValid = response.valid
+
+  if (!isValid) {
+    const searchParams = new URLSearchParams({
+      title: 'Invalid Farcaster Id',
+    })
+
+    return new NextResponse(
+      getFrameHtmlResponse({
+        buttons: [
+          {
+            label: 'Try Again',
+          },
+        ],
+        image: {
+          src: `${process.env.NEXT_PUBLIC_SITE_URL}/og?${searchParams}`,
+        },
+        postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+      })
+    )
+  }
+
   const { untrustedData } = body
 
   if (untrustedData.buttonIndex === 1) {
