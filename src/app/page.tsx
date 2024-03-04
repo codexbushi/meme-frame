18ca24a4b33f1271a78f42d9d8477e7d2880c67a
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-04 21:47:04 +0800

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,29 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <>
-      <h1>Meme Generator</h1>
-    </>
+    <div className="p-12">
+      <h1 className="text-2xl md:text-5xl text-center font-bold">
+        Meme Generator
+      </h1>
+      <h2 className="mt-4 text-xl md:text-3xl text-center font-bold">
+        A Frame Experiment by{' '}
+        <a
+          className="text-indigo-400 hover:text-indigo-300"
+          href="https://warpcast.com/hunterchang.eth"
+        >
+          Hunter Chang
+        </a>
+      </h2>
+
+      <p className="mt-10 text-xl md:text-2xl text-center font-bold">
+        Watch my video about{' '}
+        <a
+          className="text-indigo-400 hover:text-indigo-300"
+          href="https://youtu.be/g_pkATT8pYU?si=jY7Ilr7E_M6hrKJB"
+        >
+          building frames
+        </a>
+      </p>
+    </div>
   )
 }
