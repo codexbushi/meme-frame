2319bb540d24d4299862ee86b2fa01733df9f32b
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-04 20:12:48 +0800

    update site preview

---
+++ src/app/api/picker/route.ts
@@ -0,0 +1,28 @@
+import { FrameRequest, getFrameHtmlResponse } from '@coinbase/onchainkit/frame'
+import { NextRequest, NextResponse } from 'next/server'
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json()
+
+  const { untrustedData } = body
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      image: {
+        src: `${process.env.NEXT_PUBLIC_SITE_URL}/meme/a`,
+      },
+      buttons: [
+        {
+          label: 'Start Over 🔄',
+        },
+      ],
+      postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/meme`,
+    })
+  )
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req)
+}
+
+export const dynamic = 'force-dynamic'
